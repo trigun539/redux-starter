bf518092db4da8d1bbae809e4ec6e92f2f44d4a1
Edwin Perez <edwinperez539@gmail.com>
2016-08-10 18:34:24 -0400

    Finished

--- main.js
+++ main.js
@@ -1,36 +1,113 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component }             from 'react';
+import { render }                       from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider, connect }            from 'react-redux';
 
-// TODO: create a container
-
-// TODO: create a ticker component
+// ********* COMPONENT **********
 class Ticker extends Component {
-	constructor (props, context) {
-		super(props, context);
-
-		this.state = {
-			number: 0
-		};
-	}
-
 	render () {
-		const { number } = this.state;
+		const { number, increment, decrement } = this.props;
 
 		return (
 			<div>
 				<h1>{ number }</h1>
-				<button>Increment</button>
-				<button>Decrement</button>
+				<button onClick={ () => { increment(); }}>Increment</button>
+				<button onClick={ () => { decrement(); }}>Decrement</button>
 			</div>
 		);
 	}
 }
 
+class App extends Component {
+	render () {
+		return (
+			<div>
+				<TickerContainer />
+				<TickerContainer />
+				<Ticker 
+					number={ 10 } 
+					increment={ () => { console.log('pushed increment'); }}
+					decrement={ () => { console.log('pushed decrement'); }}
+					/>
+			</div>
+		);
+	}
+}
 
-// TODO: create a store
+// ACTION CONSTANTS
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
 
-// TODO: create an action
+// ACTION CREATORS
+const increment = () => {
+	return { type: INCREMENT };
+};
 
-// TODO: create a reducer
+const decrement = () => {
+	return { type: DECREMENT };
+};
 
-render(<Ticker />, document.getElementById('container'));
+// ********** THE CONTAINER *******************
+
+class TickerWrapper extends Component {
+	render () {
+		const { firstNumber, increment, decrement } = this.props;
+
+		return (
+			<Ticker 
+				number={ firstNumber } 
+				increment={ increment } 
+				decrement={ decrement } />
+		);
+	}
+}
+
+// ************ SPECIAL FUNCTIONS ****************
+const mapToState = (state) => {
+	return { firstNumber: state.firstNumber };
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		increment () { dispatch(increment()); },
+		decrement () { dispatch(decrement()); }
+	};
+}
+
+// ************ SPECIAL FUNCTIONS ****************
+const TickerContainer = connect(mapToState, mapDispatchToProps)(TickerWrapper);
+
+// ********** THE CONTAINER *******************
+
+// ********** THE REDUCER ***************
+
+const firstNumber = (state = 5, action) => {
+	switch (action.type) {
+		case INCREMENT: 
+			return ++state;
+		case DECREMENT:
+			return --state;
+		default:
+			return state;
+	}
+}
+
+const allReducers = combineReducers({
+	firstNumber
+});
+
+// *************** STORE ******************
+
+let store = createStore(allReducers);
+
+window.store = store;
+
+render(
+	<Provider store={ store }>
+		<App />
+	</Provider>
+, document.getElementById('container'));
+
+
+
+
