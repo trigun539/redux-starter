07ba24dd98fdbe11217b6432a5dd2204d9f53af0
Edwin Perez <edwinperez539@gmail.com>
2016-08-10 17:09:24 -0400

    some stuff

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
 
+// TODO: create a container
 
-console.log('webpack owrkds');
+// TODO: create a ticker component
+
+// TODO: create a store
+
+// TODO: create an action
+
+// TODO: create a reducer
+
+render(<h1>Hello world</h1>, document.getElementById('container'));
